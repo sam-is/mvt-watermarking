f4c32c098d92c41bc57dac229ddb8b68537fd168
danila <danila_glazkov@mail.ru>
2022-10-19 18:28:47 +0400

    added interface for watermark

---
+++ Watermark/Watermark/IWatermark.cs
@@ -0,0 +1,11 @@
+﻿using NetTopologySuite.IO.VectorTiles;
+using System.Collections.Generic;
+
+namespace Watermark
+{
+    public interface IWatermark
+    {
+        VectorTile Embed(VectorTile tile, byte[] message, int key, List<double> parameters);
+        byte[] Extract(VectorTile tile, int key, List<double> parameters);
+    }
+}
