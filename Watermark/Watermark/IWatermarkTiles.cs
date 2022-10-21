7e0a182518b70a3f7affc638760acb44919bc7e6
danila <danila_glazkov@mail.ru>
2022-10-21 16:10:51 +0400

    added interface for tile list

---
+++ Watermark/Watermark/IWatermarkTiles.cs
@@ -0,0 +1,12 @@
+﻿using NetTopologySuite.IO.VectorTiles;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Watermark
+{
+    public interface IWatermarkTiles
+    {
+        List<VectorTile> Embed(List<VectorTile> tiles, int key, BitArray message);
+        BitArray Extract(List<VectorTile> tile, int key);
+    }
+}
