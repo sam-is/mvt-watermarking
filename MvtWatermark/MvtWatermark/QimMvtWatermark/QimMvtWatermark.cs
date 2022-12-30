8cef85ea0b9a45ff7fc44cd21daa7dea197ca61b
danila <danila_glazkov@mail.ru>
2022-12-30 14:56:41 +0400

    clean up

--- MvtWatermark/MvtWatermark/QimMvtWatermark/QimMvtWatermark.cs
+++ MvtWatermark/MvtWatermark/QimMvtWatermark/QimMvtWatermark.cs
@@ -1,10 +1,8 @@
 ﻿using NetTopologySuite.Geometries;
 using NetTopologySuite.IO.VectorTiles;
-using NetTopologySuite.IO.VectorTiles.Mapbox;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace MvtWatermark.QimMvtWatermark;
