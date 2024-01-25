71c3d64f4acb7a717ef0de9070322d04a6995821
danlla <danila_glazkov@mail.ru>
2024-01-25 18:56:03 +0400

    added interface and classes for other extraction methods

---
+++ MvtWatermark/MvtWatermark/QimMvtWatermark/ExtractingMethods/GeneralExtractionMethod.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MvtWatermark.QimMvtWatermark.ExtractingMethods;
+public class GeneralExtractionMethod : IExtractingMethod
+{
+    public class PairOfStatistics(int s0, int s1)
+    {
+        public int S0 { get; set; } = s0;
+        public int S1 { get; set; } = s1;
+    };
+
+    public Dictionary<int, PairOfStatistics> Values { get; set; }
+    public int CountBits { get; set; }
+    public double RelativeNumber { get; set; }
+    public GeneralExtractionMethod(int countBits, double relativeNumber)
+    {
+        CountBits = countBits;
+        RelativeNumber = relativeNumber;
+        Values = new Dictionary<int, PairOfStatistics>(countBits);
+        for (var i = 0; i < countBits; i++)
+            Values.Add(i, new(0, 0));
+    }
+
+    public void AddStatistics(int index, int s0, int s1)
+    {
+        Values[index].S0 += s0;
+        Values[index].S1 += s1;
+    }
+
+    public BitArray GetBits()
+    {
+        var bits = new BitArray(CountBits, false);
+        for (var i = 0; i < CountBits; i++)
+        {
+            var s0 = Values[i].S0;
+            var s1 = Values[i].S1;
+            if ((double)Math.Abs(s0 - s1) / (s1 + s0) > RelativeNumber)
+            {
+                if (s1 > s0)
+                    bits[i] = true;
+                if (s0 > s1)
+                    bits[i] = false;
+            }
+        }
+
+        return bits;
+    }
+}
---
+++ MvtWatermark/MvtWatermark/QimMvtWatermark/ExtractingMethods/MajorityExtractionMethod.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MvtWatermark.QimMvtWatermark.ExtractingMethods;
+public class MajorityExtractionMethod : IExtractingMethod
+{
+    public Dictionary<int, int> Values { get; set; }
+    public int CountBits { get; set; }
+    public MajorityExtractionMethod(int countBits)
+    {
+        CountBits = countBits;
+        Values = new Dictionary<int, int>(countBits);
+        for (var i = 0; i < countBits; i++)
+            Values.Add(i, 0);
+    }
+
+    public void AddStatistics(int index, int s0, int s1)
+    {
+        if (s0 > s1)
+            Values[index] -= 1;
+
+        if (s1 > s0)
+            Values[index] += 1;
+    }
+
+    public BitArray GetBits()
+    {
+        var bits = new BitArray(CountBits, false);
+        for (var i = 0; i < CountBits; i++)
+        {
+            if (Values[i] > 0)
+                bits[i] = true;
+            if (Values[i] < 0)
+                bits[i] = false;
+        }
+
+        return bits;
+    }
+}
