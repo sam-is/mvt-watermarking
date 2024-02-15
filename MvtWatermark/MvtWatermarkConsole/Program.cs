f5f7cda7f7b200c4dda70cbf7b19f5e8baf592fc
danlla <danila_glazkov@mail.ru>
2024-02-15 19:41:14 +0400

    fixed help

--- MvtWatermark/MvtWatermarkConsole/Program.cs
+++ MvtWatermark/MvtWatermarkConsole/Program.cs
@@ -3,6 +3,8 @@ using CommandLine.Text;
 using MvtWatermark.QimMvtWatermark;
 using MvtWatermarkConsole.Model;
 using MvtWatermarkConsole.Readers;
+using MvtWatermarkConsole.Writers;
+using System.Reflection;
 using System.Text;
 
 namespace MvtWatermarkConsole;
@@ -13,40 +15,45 @@ internal class Program
     {
         var parser = new Parser(with =>
         {
-            with.CaseInsensitiveEnumValues = true;
-            with.AutoHelp = true;
-            with.HelpWriter = Console.Out;
+            with.HelpWriter = null;
         });
 
         var res = parser.ParseArguments<Options>(args);
 
+        var headingInfo = new HeadingInfo(programName: "MvtWatermark", version: Assembly.GetExecutingAssembly().GetName().Version?.ToString());
+
         if (res.Errors.Any())
+        {
+            var builder = SentenceBuilder.Create();
+            var errorMessages = HelpText.RenderParsingErrorsTextAsLines(res, builder.FormatError, builder.FormatMutuallyExclusiveSetErrors, 1);
+            Console.WriteLine(GenerateHelpText(res, headingInfo).AddPreOptionsLines(errorMessages));
             return;
+        }
 
         var options = res.Value;
 
-        if (options.Mode == Model.Mode.Embed && (options.Message == null || options.OutputPath == null))
+        if (options.Mode == Model.Mode.Embed && (options.Watermark == null || options.OutputPath == null))
         {
             var message = new StringBuilder();
-            if (options.Message == null)
-                message.Append("watermark parameter must be exist for embeding mode");
+            if (options.Watermark == null)
+                message.Append("Watermark parameter must be exist for embeding mode");
 
             if (options.OutputPath == null)
-                message.Append($"{(message.Length == 0 ? "" : "\n")} output parameter must be exist for embeding mode");
+                message.Append($"{(message.Length == 0 ? "" : "\n")} Output parameter must be exist for embeding mode");
 
-            Console.WriteLine(HelpText.AutoBuild(res, _ => _, _ => _).AddPreOptionsText($"\n{message}"));
+            Console.WriteLine(GenerateHelpText(res, headingInfo).AddPreOptionsText($"\n{message}"));
             return;
         }
 
         if (!File.Exists(options.Source) && !Directory.Exists(options.Source))
         {
-            Console.WriteLine(HelpText.AutoBuild(res, _ => _, _ => _).AddPreOptionsText($"\nnot exist source: {options.Source} "));
+            Console.WriteLine(GenerateHelpText(res, headingInfo).AddPreOptionsText($"\nNot exist source: {options.Source} "));
             return;
         }
 
         if (options.ConfigPath != null && !File.Exists(options.ConfigPath))
         {
-            Console.WriteLine(HelpText.AutoBuild(res, _ => _, _ => _).AddPreOptionsText($"\nnot exist config file, but select: {options.Source} "));
+            Console.WriteLine(GenerateHelpText(res, headingInfo).AddPreOptionsText($"\nNot exist config file, but select: {options.Source} "));
             return;
         }
 
@@ -56,7 +63,7 @@ internal class Program
         }
         catch (Exception ex)
         {
-            Console.WriteLine(HelpText.AutoBuild(res, _ => _, _ => _).AddPreOptionsText($"exception: {ex.Message}"));
+            Console.WriteLine(GenerateHelpText(res, headingInfo).AddPreOptionsText($"Exception: {ex.Message}"));
         }
     }
 
@@ -70,14 +77,28 @@ internal class Program
         switch (options.Mode)
         {
             case Model.Mode.Embed:
-                var watermarked = watermark.Embed(data, options.Key, MessageTransform.GetBitArray(options.Message!));
-                Writers.DataWriter.Write(watermarked, options.OutputPath!);
+                var watermarked = watermark.Embed(data, options.Key, MessageTransform.GetBitArray(options.Watermark!));
+                DataWriter.Write(watermarked, options.OutputPath!);
                 break;
 
             case Model.Mode.Extract:
                 var message = watermark.Extract(data, options.Key);
-                Console.WriteLine(MessageTransform.GetMessage(message));
+                if (options.OutputPath != null)
+                    MessageWriters.Write(options.OutputPath, MessageTransform.GetMessage(message));
+                else
+                    Console.WriteLine(MessageTransform.GetMessage(message));
                 break;
         }
+    }
+
+    private static HelpText GenerateHelpText(ParserResult<Options> result, HeadingInfo headingInfo)
+    {
+        return HelpText.AutoBuild(result, h =>
+        {
+            h.AdditionalNewLineAfterOption = true;
+            h.Heading = headingInfo;
+            h.Copyright = "Copyright (c) Samara-Informsputnik";
+            return h;
+        }, e => e);
     }
 }
