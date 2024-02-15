fbf81aaad62b8b84aacba0bba51e9603b631346c
danlla <danila_glazkov@mail.ru>
2024-02-15 19:38:45 +0400

    added class for writing message

---
+++ MvtWatermark/MvtWatermarkConsole/Writers/MessageWriters.cs
@@ -0,0 +1,9 @@
+﻿namespace MvtWatermarkConsole.Writers;
+public static class MessageWriters
+{
+    public static void Write(string path, string message)
+    {
+        using var writer = new StreamWriter(path);
+        writer.WriteLine(message);
+    }
+}
