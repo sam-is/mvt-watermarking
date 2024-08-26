a1d8c186e3753aa13b779d5b3c91dcc96d751a4d
danlla <danila_glazkov@mail.ru>
2024-08-26 19:11:39 +0400

    updated options help text

--- MvtWatermark/MvtWatermarkConsole/Model/Options.cs
+++ MvtWatermark/MvtWatermarkConsole/Model/Options.cs
@@ -25,7 +25,7 @@ public class Options
         Output path to save. 
         For mode Embed, where to save watermarked data. 
         For mode Extract, where to save watermarked message.
-        For Embed required. If the source parameter is a mbtiles and the output parameter is not set, it will overwrite the tiles in the source.
+        For Embed required. If the source parameter is a mbtiles and the output parameter is not set, the tiles in the source will be overwritten.
         For extract optional, if not select watermark print into console.
         """)]
     public string? OutputPath { get; set; }
@@ -36,12 +36,12 @@ public class Options
     [Option("maxz", HelpText = "Optional. Maximum zoom.")]
     public int? MaxZ { get; set; }
 
-    [Option('g', "generate-config", HelpText = "Optional. If selected on Embed mode, it will generate a config file on path from the config parameter.")]
+    [Option('g', "generate-config", HelpText = "Optional. If selected on Embed mode, config file will be generated on path from the config parameter.")]
     public bool IsGenerateConfig { get; set; }
 
-    [Option('u', "update-config", HelpText = "Optional. If selected on Embed mode, it will write length of message in the config file on path from the config parameter.")]
+    [Option('u', "update-config", HelpText = "Optional. If selected on Embed mode, length of message will be written or rewritten in the config file on path from the config parameter.\r\n")]
     public bool IsUpdateConfig { get; set; }
 
-    [Option("no-tile-compression", HelpText = "Optional. Select if tile in source without compression")]
+    [Option("no-tile-compression", HelpText = "Optional. Select if tiles in source without compression")]
     public bool IsNoCompression { get; set; }
 }
